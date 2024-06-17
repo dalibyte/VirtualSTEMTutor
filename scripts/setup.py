cd8f278069ce2fd90e382575195dd68ef60d6c12
Jonathan Stewart <128931852+dalibyte@users.noreply.github.com>
2024-06-16 20:28:57 -0400

    Update setup.py

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,9 +1,8 @@
 from nemo.collections.nlp.models import QAModel, SummarizationModel
 from nemo.collections.nlp.data.datasets import QADataSet
 
-# NeMo dataset preparation
-dataset = QADataSet.from_text_files(["../data/textbooks.txt", 
-"../data/lecture_notes.txt"])
+# Prepare the dataset
+dataset = QADataSet.from_text_files(["../data/textbooks.txt", "../data/lecture_notes.txt"])
 
 # Fine-tune QA model
 qa_model = QAModel.from_pretrained("nvidia/qa_model")
@@ -11,8 +10,6 @@ qa_model.train(dataset)
 qa_model.save_pretrained("../models/fine_tuned_qa_model")
 
 # Fine-tune Summarization model
-summarization_model = 
-SummarizationModel.from_pretrained("nvidia/summarization_model")
+summarization_model = SummarizationModel.from_pretrained("nvidia/summarization_model")
 summarization_model.train(dataset)
 summarization_model.save_pretrained("../models/fine_tuned_summarization_model")
-
